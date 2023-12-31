d8046c5d283a19eb84ccefca79059decff1f5efd
katjam <katjamordaunt@gmail.com>
2023-12-31 13:46:12 +0000

    feat: Add main text to top of sections 6 & 11

--- src/elm/View/Section11.elm
+++ src/elm/View/Section11.elm
@@ -4,10 +4,12 @@ import Data
 import Html
 import Model exposing (Model)
 import Msg exposing (Msg)
+import View.MainText
 import View.Posts
 
 
 view : Model -> List (Html.Html Msg)
 view model =
-    [ View.Posts.view Data.Section11 model.content.posts
+    [ View.MainText.viewTop Data.Section11 model.content.mainText
+    , View.Posts.view Data.Section11 model.content.posts
     ]
--- src/elm/View/Section6.elm
+++ src/elm/View/Section6.elm
@@ -3,9 +3,11 @@ module View.Section6 exposing (view)
 import Html
 import Model exposing (Model)
 import Msg exposing (Msg)
+import View.MainText
 
 
 view : Model -> List (Html.Html Msg)
 view model =
     [ Html.h2 [] [ Html.text "Section 6 - terminal" ]
+    , View.MainText.viewTop Data.Section6 model.content.mainText
     ]
