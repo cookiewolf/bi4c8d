f9ddac933ee2d30ee3a47b8ae7f29ba54eea867e
katjam <katjamordaunt@gmail.com>
2024-02-02 15:22:04 +0000

    Improve layout & add info faces

--- src/elm/View/Section5.elm
+++ src/elm/View/Section5.elm
@@ -11,7 +11,8 @@ import Msg exposing (Msg(..))
 
 type alias FadeImage =
     { id : String
-    , src : String
+    , srcId : Int
+    , isBlank : Bool
     , opacity : String
     , scale : String
     }
@@ -20,52 +21,90 @@ type alias FadeImage =
 view : Model -> List (Html.Html Msg)
 view model =
     [ Html.div [ Html.Attributes.class "faces-with-info" ]
-        (List.indexedMap
-            (\index imageSrc ->
+        (List.map
+            (\imageSrcId ->
                 let
                     itemId =
-                        "fade-image-" ++ String.fromInt (index + 1)
+                        "fade-image-" ++ String.fromInt imageSrcId
 
-                    ( itemOpacity, itemScale ) =
-                        case InView.isInViewWithMargin itemId (InView.Margin 100 0 100 0) model.inView of
+                    isBlank =
+                        case InView.isInOrAboveViewWithMargin itemId (InView.Margin 200 0 400 0) model.inView of
                             Just True ->
-                                ( "1", "1" )
+                                False
 
                             _ ->
-                                ( "0", "0.5" )
+                                True
+
+                    isJustInView =
+                        case InView.isInViewWithMargin itemId (InView.Margin 200 0 400 0) model.inView of
+                            Just True ->
+                                False
+
+                            _ ->
+                                True
                 in
                 viewImage model.inView
                     { id = itemId
-                    , src = imageSrc
-                    , opacity = itemOpacity
-                    , scale = itemScale
+                    , srcId = imageSrcId
+                    , isBlank = isBlank
+                    , opacity =
+                        if isBlank then
+                            "1"
+
+                        else
+                            "1"
+                    , scale =
+                        if isBlank then
+                            "0.5"
+
+                        else
+                            "1"
                     }
             )
-            imageSrcList
+            (List.range 1 5)
         )
     ]
 
 
 viewImage : InView.State -> FadeImage -> Html.Html Msg
 viewImage state fadeImage =
-    Html.img
-        [ Html.Attributes.id fadeImage.id
-        , Html.Attributes.src fadeImage.src
-        , Html.Events.on "load" (Json.Decode.succeed (OnElementLoad fadeImage.id))
-        , Html.Attributes.style "opacity" fadeImage.opacity
-        , Html.Attributes.style "max-width" "100%"
-
-        -- , style "height" "100%"
-        , Html.Attributes.style "transition" "opacity 15s , transform 10s"
-        , Html.Attributes.style "transform" ("scale(" ++ fadeImage.scale ++ ")")
+    Html.div
+        [ Html.Attributes.style "display" "flex"
+        , Html.Attributes.style "flex-direction" "row"
         ]
-        []
+        [ Html.div [ Html.Attributes.class "image-wrapper" ]
+            [ Html.img
+                [ Html.Attributes.id fadeImage.id
+                , Html.Attributes.src (imageSrcFromId fadeImage.isBlank fadeImage.srcId)
+                , Html.Events.on "load" (Json.Decode.succeed (OnElementLoad fadeImage.id))
+                , Html.Attributes.style "max-width" "100%"
+                , Html.Attributes.style "opacity" fadeImage.opacity
+                , Html.Attributes.style "transition" (imageTransitionFromId fadeImage.isBlank fadeImage.srcId)
+                , Html.Attributes.style "transform" ("scale(" ++ fadeImage.scale ++ ")")
+                ]
+                []
+            ]
+        , Html.div [ Html.Attributes.class "profile-info" ] [ Html.text "profile info" ]
+        ]
 
 
-imageSrcList =
-    [ "/images/info-faces/001.jpg"
-    , "/images/info-faces/002.jpg"
-    , "/images/info-faces/003.jpg"
-    , "/images/info-faces/004.jpg"
-    , "/images/info-faces/005.jpg"
-    ]
+imageTransitionFromId : Bool -> Int -> String
+imageTransitionFromId isBlank srcId =
+    if isBlank then
+        "opacity 0s, transform 0s"
+
+    else
+        "opacity 10s, transform 20s"
+
+
+imageSrcFromId : Bool -> Int -> String
+imageSrcFromId isBlank srcId =
+    "/images/info-faces/00"
+        ++ String.fromInt srcId
+        ++ (if isBlank then
+                "-white.jpg"
+
+            else
+                ""
+                    ++ ".jpg"
+           )
