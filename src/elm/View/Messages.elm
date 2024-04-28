6439644854d19a5f4d0ada843a7362ee402bda2b
katjam <katjamordaunt@gmail.com>
2024-04-28 20:02:28 +0100

    Animate message loading

--- src/elm/View/Messages.elm
+++ src/elm/View/Messages.elm
@@ -6,6 +6,9 @@ import Html.Attributes
 import InView
 import Markdown
 import Msg exposing (Msg)
+import Simple.Animation
+import Simple.Animation.Animated
+import Simple.Animation.Property
 import Time
 
 
@@ -16,9 +19,10 @@ view inViewState sectionId messageList title =
             [ Html.h3 [] [ Html.text title ]
             , Html.div [ Html.Attributes.class "messages-inner" ]
                 (if sectionInView inViewState sectionId then
-                    List.map
-                        (\message ->
-                            viewMessage message
+                    List.indexedMap
+                        (\index message ->
+                            viewMessage index
+                                message
                                 (isLastAtTime message messageList)
                         )
                         (Data.filterBySection sectionId messageList)
@@ -59,23 +63,82 @@ isLastAtTime message allMessages =
             False
 
 
-viewMessage : Data.Message -> Bool -> List (Html.Html Msg)
-viewMessage message isLast =
+viewMessage : Int -> Data.Message -> Bool -> List (Html.Html Msg)
+viewMessage messageIndex message isLast =
     [ Html.div
-        [ Html.Attributes.class "message"
+        [ Html.Attributes.class "message-bubble"
         , Html.Attributes.class (Data.sideToString message.side)
         ]
-        [ Html.div
-            [ Html.Attributes.class "message-body"
+        [ Simple.Animation.Animated.div
+            (fadeIn (fadeInDelay messageIndex - 1200))
+            [ Html.Attributes.class "typing-dots" ]
+            [ viewTypingDot 1
+            , viewTypingDot 2
+            , viewTypingDot 3
             ]
-            (Markdown.markdownToHtml message.body)
-        , if isLast then
-            Html.div [ Html.Attributes.class "message-time" ] [ viewMessageTime message.datetime ]
+        , Simple.Animation.Animated.div
+            (fadeIn (fadeInDelay messageIndex))
+            [ Html.Attributes.class "message"
+            ]
+            [ Html.div
+                [ Html.Attributes.class "message-body"
+                ]
+                (Markdown.markdownToHtml message.body)
+            , if isLast then
+                Html.div [ Html.Attributes.class "message-time" ] [ viewMessageTime message.datetime ]
 
-          else
-            Html.text ""
+              else
+                Html.text ""
+            ]
         ]
     ]
+
+
+fadeInDelay : Int -> Int
+fadeInDelay messageIndex =
+    3000 * messageIndex
+
+
+expandFade : Int -> Simple.Animation.Animation
+expandFade delay =
+    Simple.Animation.fromTo
+        { duration = 1000
+        , options =
+            [ Simple.Animation.loop
+            , Simple.Animation.delay delay
+            ]
+        }
+        [ Simple.Animation.Property.opacity 1
+        , Simple.Animation.Property.scale 1
+        ]
+        [ Simple.Animation.Property.opacity 0
+        , Simple.Animation.Property.scale 1.2
+        ]
+
+
+fadeIn : Int -> Simple.Animation.Animation
+fadeIn delay =
+    Simple.Animation.fromTo
+        { duration = 100
+        , options =
+            [ Simple.Animation.delay delay
+            ]
+        }
+        [ Simple.Animation.Property.opacity 0 ]
+        [ Simple.Animation.Property.opacity 1 ]
+
+
+viewTypingDot position =
+    Simple.Animation.Animated.div (expandFade (position * 100))
+        [ Html.Attributes.class "dot"
+        , Html.Attributes.style "margin-right" "10px"
+        ]
+        [ Html.div
+            [ Html.Attributes.class "dot"
+            , Html.Attributes.style "margin-right" "8px"
+            ]
+            []
+        ]
 
 
 viewMessageTime : Time.Posix -> Html.Html Msg
