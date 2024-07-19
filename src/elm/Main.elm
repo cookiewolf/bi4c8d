c6013ddb3e646b29331e0d9e18482b6088c8da93
aaaaargZombies <6824891+aaaaargZombies@users.noreply.github.com>
2024-07-19 11:41:24 +0100

    tidy up Main

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -15,8 +15,6 @@ import Random
 import Task
 import Time
 import View
-import View.Posts
-import View.StackingImage
 
 
 port onScroll : ({ x : Float, y : Float } -> msg) -> Sub msg
@@ -56,15 +54,12 @@ init flags =
             Data.decodedContent flags
 
         section1draggableContent =
-            -- ( Data.Section1, View.Posts.posts Data.Section1 content.posts )
             ( Data.Section1, content.posts |> Data.filterBySection Data.Section1 |> List.map Pile.Post )
 
         section4draggableContent =
-            -- ( Data.Section4, View.StackingImage.viewImageListDraggable Data.Section4 content.images )
             ( Data.Section4, content.images |> Data.filterBySection Data.Section4 |> List.map Pile.Image )
 
         section10draggableContent =
-            -- ( Data.Section10, View.Posts.posts Data.Section10 content.posts )
             ( Data.Section10, content.posts |> Data.filterBySection Data.Section10 |> List.map Pile.Post )
     in
     ( { time = Time.millisToPosix 0
@@ -77,11 +72,6 @@ init flags =
       , chartHovering = []
       , terminalState = { input = "", history = [] }
       , piles = Pile.init [ section1draggableContent, section4draggableContent, section10draggableContent ]
-
-      -- , piles = Pile.init [ section1draggableContent, section4draggableContent, section10draggableContent ]
-      -- , pile1 = Pile.init (View.Posts.posts Data.Section1 (Data.decodedContent flags |> (\c -> c.posts)))
-      -- , pile2 = Pile.init (View.StackingImage.viewImageListDraggable Data.Section4 (Data.decodedContent flags |> (\c -> c.images)))
-      -- , pile3 = Pile.init (View.Posts.posts Data.Section10 (Data.decodedContent flags |> (\c -> c.posts)))
       }
     , Cmd.batch
         [ Random.generate NewRandomIntList generateRandomIntList
